1b2607adf2340012aadd7d553e323a7ad28fe9d2
Benjamin <benjamin.jugendfeuerwehr@gmail.com>
2023-10-01 19:05:44 +0200

    ad

---
+++ D3.js/script.js
@@ -0,0 +1,26 @@
+const DUMMY_DATA = [
+  {id: 'd1', value: 10, region: 'USA'},
+  {id: 'd2', value: 11, region: 'India'},
+  {id: 'd3', value: 12, region: 'China'},
+  {id: 'd4', value: 6, region: 'Germany'},
+];
+
+const container = d3.select('div')
+  .classed('container', true)
+  .style('border', '1px solid red');
+
+const bars = container.selectAll('div')
+  .data(DUMMY_DATA)
+  .enter()
+  .append('div')
+  .classed('bar', true)
+  .style('width', '50px')
+  .style('height', '150px');
+
+
+//d3.select('div')
+//  .select('p')
+//  .data(DUMMY_DATA)
+//  .enter()
+//  .append('p')
+//  .text(dta => dta.region);
